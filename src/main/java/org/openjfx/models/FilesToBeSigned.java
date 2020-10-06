c448b3b1abedb2f476206fd614ed48b98779e494
fkmurphy <njmdistrisoft@gmail.com>
2020-10-06 11:49:40 -0300

    thread backend and status column

--- src/main/java/org/openjfx/models/FilesToBeSigned.java
+++ src/main/java/org/openjfx/models/FilesToBeSigned.java
@@ -1,19 +1,36 @@
 package org.openjfx.models;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import org.kordamp.ikonli.javafx.FontIcon;
 import org.openjfx.file.FileRepository;
 
 public class FilesToBeSigned {
     protected FileRepository file;
     protected CheckBox checked;
+    protected Button signed;
+
+    //
     public FilesToBeSigned(FileRepository file){
         this.file = file;
         this.checked = new CheckBox();
+        this.signed = new Button();
+        setStatus("");
+        signed.setStyle(
+                "-fx-background-color:none;"+
+                        "-fx-border:none;"
+        );
     }
     public FilesToBeSigned(FileRepository file, Boolean checked){
         this.file = file;
         this.checked = new CheckBox();
         this.checked.setSelected(checked);
+        this.signed = new Button();
+        setStatus("");
+        signed.setStyle(
+                "-fx-background-color:none;"+
+                        "-fx-border:none;"
+        );
     }
 
     public FileRepository getFile()
@@ -33,8 +50,8 @@ public class FilesToBeSigned {
         return this.checked;
     }
 
-    public void setChecked(CheckBox checked){
-        this.checked = checked;
+    public void setChecked(boolean checked){
+        this.checked.setSelected(checked);
     }
 
     @Override
@@ -42,6 +59,32 @@ public class FilesToBeSigned {
         if(obj == null || getClass() != obj.getClass())
             return false;
         return this.getRepresentativePath().compareTo(((FilesToBeSigned)obj).getRepresentativePath()) == 0;
-
     }
+
+    public void setStatus(String status)
+    {
+        FontIcon icon;
+        switch (status) {
+            case "signed":
+                icon = new FontIcon("fa-check");
+                break;
+            case "fail":
+                icon = new FontIcon("fa-close");
+            default:
+                icon = new FontIcon("fas-file-signature");
+                break;
+        }
+        icon.setIconSize(15);
+        this.signed.setGraphic(icon);
+    }
+
+    public void setSigned (Button btn)
+    {
+        this.signed = btn;
+    }
+    public Button getSigned ()
+    {
+        return this.signed;
+    }
+
 }
