89fc34706c2f0c319cf4afc990b300b0ae5ee6b7
fkmurphy <njmdistrisoft@gmail.com>
2020-10-16 11:57:20 -0300

    environment variable

--- src/main/java/org/openjfx/MainApp.java
+++ src/main/java/org/openjfx/MainApp.java
@@ -3,6 +3,9 @@ package org.openjfx;
 public class MainApp  {
 
     public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
         HelloFX.main(args);
     }
 
