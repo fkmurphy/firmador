df2980c4bf2815322728855445db0bf3e7484ffd
fkmurphy <njmdistrisoft@gmail.com>
2020-10-16 11:58:50 -0300

    remove printlns

--- src/main/java/org/openjfx/MainApp.java
+++ src/main/java/org/openjfx/MainApp.java
@@ -3,9 +3,6 @@ package org.openjfx;
 public class MainApp  {
 
     public static void main(String[] args) {
-        for (String arg : args) {
-            System.out.println(arg);
-        }
         HelloFX.main(args);
     }
 
